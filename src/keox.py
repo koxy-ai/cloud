5819ec5d06235e4df5aed5233f3903b54043a0b5
kais.radwan.personal <kais.radwan.personal@gmail.com>
2024-09-09 21:33:50 +0000

    few changes

--- src/keox.py
+++ src/keox.py
@@ -9,10 +9,11 @@ class Keox:
             modal.Image.debian_slim(python_version="3.11.8")
             .apt_install("git")
             .apt_install("curl")
+            .apt_install("unzip")
             .run_commands(
                 # "curl -fsSL https://deno.land/x/install/install.sh | sh",
                 "git clone https://github.com/koxy-ai/cloud /source",
-                "echo 'READY'"
+                "echo 'READY NOW'"
             )
         )
 
